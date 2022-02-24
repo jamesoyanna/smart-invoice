36ea6b2e562addf62e90789056e7eda5868f264c
James <engrjayt200@gmail.com>
2022-02-24 20:27:41 +0100

    Add api config file

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -7,5 +7,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://smart-invoice.herokuapp.com"
+    baseURL: "https://smart-invoice-backend.herokuapp.com"
 })
+
+// https://smart-invoice-backend.herokuapp.com
